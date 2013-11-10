f7ef42f982e718a1c429bdbcb98ed55536f8c8f9
Ty Lewis <tyglewis@gmail.com>
2013-11-09 18:13:40 -0700

    Adding occ grid query.

---
+++ src/ServerResponse/OccGridResponse.java
@@ -0,0 +1,14 @@
+package ServerResponse;
+
+import java.awt.geom.Point2D;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ty
+ * Date: 11/9/13
+ * Time: 5:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OccGridResponse {
+    boolean [][] occupiedObservation;
+}
